0ab045a60e0bd8d844d3627a14ec5e1ba8b64226
Ber-08 <bezytryphena.com>
2024-05-03 10:53:13 +0530

    03-05-24

--- my-joke-app/src/App.js
+++ my-joke-app/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import Joke from "./Joke";
+import Jokes from "./Joke";
 
 function App() {
   return (
     <div className="App">
       <h1>This is the joke generator webpage</h1>
-      <Joke />
+      <Jokes />
     </div>
   );
 }
--- my-joke-app/src/Joke.jsx
+++ my-joke-app/src/Joke.jsx
@@ -1,23 +1,20 @@
 import React, { useState } from "react";
 
-const Joke = () => {
-  const [data, setData] = useState("");
+const Jokes = () => {
+  const [joke, setJoke] = useState("");
 
   const fetchApi = () => {
     fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
       .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setData(data.joke);
-      });
+      .then((data) => setJoke(data.joke));
   };
 
   return (
     <div>
       <button onClick={fetchApi}>Click here!</button>
-      <p>{data}</p>
+      <p>{joke}</p>
     </div>
   );
 };
 
-export default Joke;
+export default Jokes;
